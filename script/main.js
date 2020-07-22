7249a9ba431309f5b59e31ce693758adcc2db1c7
Golvin001 <68605933+Golvin001@users.noreply.github.com>
2020-07-22 09:42:34 -0400

    Add files via upload

---
+++ script/main.js
@@ -0,0 +1,30 @@
+let myImage = document.querySelector('img');
+
+myImage.onclick = function() {
+    let mySrc = myImage.getAttribute('src');
+    if(mySrc === 'image/test-image-fantasy-art.jpg') {
+      myImage.setAttribute ('src','image/test-image-road.jpg');
+    } else {
+      myImage.setAttribute ('src','image/test-image-fantasy-art.jpg');
+    }
+}
+let myButton = document.querySelector('button');
+let myHeading = document.querySelector('h1');    
+function setUserName() {
+    let myName = prompt('Please enter your name.');
+    if(!myName) {
+      setUserName();
+    } else {
+      localStorage.setItem('name', myName);
+      myHeading.innerHTML = 'Mozilla is cool, ' + myName;
+    }
+  }
+  if(!localStorage.getItem('name')) {
+    setUserName();
+  } else {
+    let storedName = localStorage.getItem('name');
+    myHeading.textContent = 'Mozilla is cool, ' + storedName;
+  }
+  myButton.onclick = function() {
+    setUserName();
+  }
